8a5e5460edc1c83ce53f8cefcd0fbc4007fbea33
nightmare-fuel-1997 <mostafagholamnataj@gmail.com>
2025-12-02 15:59:44 +0330

    added custom userModel and test for it

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,57 @@
-from django.db import models # noqa
+"""
+Databse models for the application.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+# AbstractBaseUser provides the core implementation of a user model without any fields.
+# permissionsMixin adds the fields and methods necessary to support Django's permission framework.
+# PermissionsMixin includes fields like is_superuser and methods for checking permissions.
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Custom user manager to handle user creation with email as the unique identifier.
+    """
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        # set_password() hashes the password and stores it securely.
+        # it comes from AbstractBaseUser.
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model that uses email as the unique identifier.
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+# This tells Django: "Use email to log in, NOT username!"
+    def __str__(self):
+        return self.email
