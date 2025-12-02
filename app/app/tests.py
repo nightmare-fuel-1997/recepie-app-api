2318b4e3d7f304c1e8a2f937d7083d4c1c09885c
nightmare-fuel-1997 <mostafagholamnataj@gmail.com>
2025-12-02 12:58:46 +0330

    test for db

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(2, 3)
+        self.assertEqual(res, 5)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(5, 3)
+        self.assertEqual(res, 2)
